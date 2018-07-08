b6719ccd75542d45fa127f5b0a01071d38b43490
cheulong <searcheulong@gmail.com>
2018-07-08 22:49:31 +0700

    final

--- react-native-boilerplate/Firebase.js
+++ react-native-boilerplate/Firebase.js
@@ -2,12 +2,7 @@ import firebase from 'firebase/app';
 import 'firebase/database';
 
 const config = {
-    apiKey: 'AIzaSyCh33nqeqhbjvJOs49Ze-zOeMJ_lDG584A',
-    authDomain: 'react-data-2d8e3.firebaseapp.com',
-    databaseURL: 'https://react-data-2d8e3.firebaseio.com',
-    projectId: 'react-data-2d8e3',
-    storageBucket: 'react-data-2d8e3.appspot.com',
-    messagingSenderId: '460561812208'
+        // FIREBASE INFO
 };
 firebase.initializeApp(config);
 export const database = firebase.database();
